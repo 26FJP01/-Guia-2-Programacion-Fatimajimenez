46f9e49d9c1c4d5de2d517cfb077c5855c0b63d1
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-07-06 21:56:06 -0600

    Update Leccion31_Actividad2.cs

--- Leccion31_Actividad2.cs
+++ Leccion31_Actividad2.cs
@@ -2,8 +2,10 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("=============Nombre:Fatima jimenez==========");
+        Console.WriteLine("=============Grado Seccio:IVC===============");
+        Console.WriteLine("=============Clave:15=======================");
         int[,] tabla = new int[4, 3];
-
         for (int fila = 0; fila < 4; fila++)
         {
             int suma = 0;
