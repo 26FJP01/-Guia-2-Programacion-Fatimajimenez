9588e9d9c136f21466a811ab7182521fc49f4328
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-07-06 21:29:35 -0600

    Add files via upload

---
+++ Leccion31_Actividad3.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] tabla = new int[4, 4];
+
+        for (int fila = 0; fila < 4; fila++)
+        {
+            for (int columna = 0; columna < 4; columna++)
+            {
+                Console.Write("Ingrese un número [{fila + 1},{columna + 1}]: ");
+                tabla[fila, columna] = int.Parse(Console.ReadLine());
+            }
+        }
+        int mayor = tabla[0, 0];
+        int filaMayor = 0;
+        int columnaMayor = 0;
+        for (int fila = 0; fila < 4; fila++)
+        {
+            for (int columna = 0; columna < 4; columna++)
+            {
+                if (tabla[fila, columna] > mayor)
+                {
+                    mayor = tabla[fila, columna];
+                    filaMayor = fila;
+                    columnaMayor = columna;
+                }
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("El número mayor es: " + mayor);
+        Console.WriteLine("Pertenece a la fila: " + (filaMayor + 1));
+        Console.WriteLine("Corresponde a la columna: " + (columnaMayor + 1));
+        Console.ReadKey();
+    }
+}
---
+++ Leccion31_Actividad3.psc
@@ -0,0 +1,26 @@
+Algoritmo Leccion31_Actividad3
+	Definir fila, columna, x, yy, mayorr Como Entero
+	Definir tabla Como Entero
+	Dimension tabla[4,4]
+	Para fila <- 1 Hasta 4 Hacer
+		Para columna <- 1 Hasta 4 Hacer
+			Escribir "Ingrese un número:"
+			Leer tabla[fila,columna]
+		FinPara
+	FinPara
+	mayorr = tabla[1,1]
+	x=1
+	yy <- 1
+	Para fila = 1 Hasta 4 Hacer
+		Para columna = 1 Hasta 4 Hacer
+			Si tabla[fila,columna] > mayorr Entonces
+				mayorr = tabla[fila,columna]
+				x = fila
+				yy = columna
+			Finsi
+		FinPara
+	FinPara
+	Escribir "El numero mayor es: ", mayorr
+	Escribir 'Pertenece a la fila ", x
+	Escribir "Y corresponde a la columna ", yy
+FinAlgoritmo
