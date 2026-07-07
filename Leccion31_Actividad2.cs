66533adb93fed05add2184dd28919510883ef1c4
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-07-06 21:22:56 -0600

    Add files via upload

---
+++ Leccion31_Actividad2.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] tabla = new int[4, 3];
+
+        for (int fila = 0; fila < 4; fila++)
+        {
+            int suma = 0;
+            for (int columna = 0; columna < 3; columna++)
+            {
+                Console.Write($"Ingrese un número [{fila + 1},{columna + 1}]: ");
+                tabla[fila, columna] = int.Parse(Console.ReadLine());
+                suma += tabla[fila, columna];
+            }
+            Console.WriteLine("La suma de la fila " + (fila + 1) + " es: " + suma);
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Leccion31_Actividad2.psc
@@ -0,0 +1,15 @@
+Algoritmo Leccion31_Actividad2
+	Definir fila, columna, nume, suma Como Entero
+	Definir tabla Como Entero
+	Dimension tabla[4,3]
+	Para fila = 1 Hasta 4 Hacer
+		suna =0
+		Para columna <- 1 Hasta 3 Hacer
+			Escribir "Ingresa un numero:"
+			Leer nume
+			tabla[fila,columna] = nume
+			sume = suma + nume
+		FinPara
+		Escribir "La sume de la fila ", fila, " en: ", suma
+	FinPara
+FinAlgoritmo
